2e5fd4b75f7a33b3f5bdc12ced29c211b0c2abcd
Timo Stroick <Timo.Stroick@hotmail.de>
2018-11-16 11:06:35 +0100

    Added Auto Class

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,20 @@
 package de.hhu.propra.db.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class Auto{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String marke;
+	private String farbe;
+	private int hubraum;
 }
